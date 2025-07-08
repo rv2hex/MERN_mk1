4ad6ef54e45a60a06cbc90ffe3135d6b74868a81
rv2hex <rv2xx699@gmail.com>
2025-07-08 19:17:32 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,20 +4,27 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimit from "./middleware/rateLimiter.js";
 import cors from "cors"
+import path from 'path'
 
 dotenv.config();
 
 console.log(process.env.MONGO_URI);  
 const app = express();
 const PORT = process.env.PORT || 5001;   
-
+const __dirname =path.resolve()
 
 connectDB();
 
 //middleware can be used for auth check and rate limiting!!
-app.use(cors({
-    origin:"http://localhost:5173",
-})); 
+
+if(process.env.NODE_ENV !== "production"){
+
+    app.use(cors({
+        origin:"http://localhost:5173",
+    })); 
+    
+}
+
 app.use(express.json());
 app.use(rateLimit)
 
@@ -28,10 +35,15 @@ app.use((req, res, next) => {
 
 app.use("/notes", notesRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(req, res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+});
+
+}
+
 app.listen(5001, () => {
     console.log("!! server is live !!");
 });
-
-
-
-//mongodb+srv://rv2xx699:QV8NCyTlVL7IBOYh@negacluster.dnvdhdc.mongodb.net/?retryWrites=true&w=majority&appName=NegaCluster
